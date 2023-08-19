ad9fdb7c7e88d4c19bdd4bc50e83e0bed0eafa6e
hyunwoomemo <archihw94@gmail.com>
2023-08-20 01:22:11 +0900

    feat: 위치, 날씨 api를 이용한 react-native 날씨 앱 만들기 완료

--- nomad_react_native/NomadWeather/App.js
+++ nomad_react_native/NomadWeather/App.js
@@ -3,11 +3,21 @@ import { View, StyleSheet, Text, ScrollView, ActivityIndicator } from 'react-nat
 import { Dimensions } from 'react-native';
 import * as Location from 'expo-location'
 import { useEffect, useState } from 'react';
-import {Ionicons} from '@expo/vector-icons'
+import { Fontisto } from '@expo/vector-icons'; 
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const API_KEY = '67d2af0194723966734128ec76b9a40d'
+
+const icons = {
+  Clouds: "cloudy",
+  Rain: "rains",
+  Clear: 'day-sunny',
+  Atmosphere: 'cloudy-gusts',
+  Snow: 'snow',
+  Drizzle: 'rain',
+  Thunderstorm: 'lightning'
+}
 
 export default function App() {
   const [city, setCity] = useState('Loading...')
@@ -50,7 +60,12 @@ export default function App() {
           ) : (
             days.map((day, index) =>
               <View key={index} style={styles.day}>
-                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <View style={{flexDirection: 'row', alignItems: 'center', width: "100%", justifyContent: 'space-between'}}>
+                  <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                  <Text style={styles.emoji}>
+                  <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+                  </Text>
+                </View>
                 <Text style={styles.desc}>{day.weather[0].main}</Text>
                 <Text style={styles.subDesc}>{day.weather[0].description}</Text>
               </View>
@@ -79,7 +94,6 @@ const styles = StyleSheet.create({
     color: "white"
   },
   weather: {
-    marginLeft: 20
   },
   day: {
     width: SCREEN_WIDTH,
@@ -88,13 +102,20 @@ const styles = StyleSheet.create({
     marginTop: 50,
     fontSize: 108,
     color: "white",
+    marginLeft: 20
+  },
+  emoji: {
+    marginTop: 50,
+    marginRight: 20,
   },
   desc: {
-    fontSize: 60,
-    color: "white"
+    fontSize: 50,
+    color: "white",
+    marginLeft: 20
   },
   subDesc: {
     fontSize: 30,
-    color: "white"
+    color: "white",
+    marginLeft: 20
   }
 })
