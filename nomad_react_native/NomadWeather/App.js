ed888a4dc8fa474c4dba6c551aff56c362ab4925
hyunwoomemo <archihw94@gmail.com>
2023-08-20 00:26:02 +0900

    feat: 위치, 날씨 api로 데이터 받아오기, 로딩 indicator 사용

--- nomad_react_native/NomadWeather/App.js
+++ nomad_react_native/NomadWeather/App.js
@@ -1,28 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet, Text, ScrollView } from 'react-native';
-import {Dimensions} from 'react-native';
+import { View, StyleSheet, Text, ScrollView, ActivityIndicator } from 'react-native';
+import { Dimensions } from 'react-native';
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react';
+import {Ionicons} from '@expo/vector-icons'
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
+const API_KEY = '67d2af0194723966734128ec76b9a40d'
+
 export default function App() {
+  const [city, setCity] = useState('Loading...')
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+
+    if (!granted) {
+      setOk(false);
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 }); // accuracy : 정확도
+
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false })
+    const { city, district } = location[0]
+    setCity(city)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`)
+    const json = await response.json()
+    console.log(json.daily)
+    setDays(json.daily)
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
+
   return (
     <View style={styles.container}>
+      <StatusBar style='light'/>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView contentContainerStyle={styles.weather} horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
+        {
+          days.length === 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator color="white" size="large" />
+            </View>
+          ) : (
+            days.map((day, index) =>
+              <View key={index} style={styles.day}>
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Text style={styles.desc}>{day.weather[0].main}</Text>
+                <Text style={styles.subDesc}>{day.weather[0].description}</Text>
+              </View>
+            )
+          )}
       </ScrollView>
     </View>
   );
@@ -31,28 +64,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'tomato'
+    backgroundColor: 'tomato',
+
   },
   city: {
     flex: 1.2,
     justifyContent: 'center',
     alignItems: 'center',
+    
   },
   cityName: {
     fontSize: 68,
-    fontWeight: "500"
+    fontWeight: "500",
+    color: "white"
   },
   weather: {
   },
   day: {
-    alignItems: 'center',
     width: SCREEN_WIDTH,
+    marginLeft: 20
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 108,
+    color: "white",
   },
   desc: {
-    fontSize: 60
+    fontSize: 60,
+    color: "white"
+  },
+  subDesc: {
+    fontSize: 30,
+    color: "white"
   }
 })
