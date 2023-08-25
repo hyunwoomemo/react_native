30156ee43795f148134c0de29e5ed207c31eca0d
hyunwoomemo <archihw94@gmail.com>
2023-08-25 22:49:17 +0900

    study: react-native styled-components theme 사용하기

--- nomad_react_native/React_Native_Master/movie_app/screens/Movies.js
+++ nomad_react_native/React_Native_Master/movie_app/screens/Movies.js
@@ -7,8 +7,7 @@ const Movies = ({navigation: {navigate}}) => {
     <Btn
       onPress={() => navigate('Stack', {screen: 'Three'})}
     >
-      <Title selected={false}>Movies</Title>
-      <Title selected={true}>Movies</Title>
+      <Title>Movies</Title>
     </Btn>
   )
 }
@@ -17,10 +16,11 @@ const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
 `
 
 const Title = styled.Text`
-  color: ${props => props.selected ? 'blue' : 'red'};
+  color: ${(props) => props.theme.textColor};
 `
 
 export default Movies
