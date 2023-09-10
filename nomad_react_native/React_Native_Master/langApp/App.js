145975b61c2929552387788593b58bc4e829ea62
hyunwoomemo <archihw94@gmail.com>
2023-09-10 20:25:15 +0900

    study: drag 기본, panResponder 생성하고 onPanResponderMove 이용하여 drag 구현하기

--- nomad_react_native/React_Native_Master/langApp/App.js
+++ nomad_react_native/React_Native_Master/langApp/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components/native'
-import { Animated, Dimensions, Easing, Pressable, TouchableOpacity } from 'react-native';
+import { Animated, Dimensions, Easing, PanResponder, Pressable, TouchableOpacity } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
@@ -18,45 +18,7 @@ const AnimatedBox = Animated.createAnimatedComponent(Box)
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window')
 
 export default function App() {
-  const position = useRef(new Animated.ValueXY({ x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 })).current;
-
-  const topLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: true
-  })
-
-  const bottomLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: true
-  })
-
-  const topRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: true
-  })
-
-  const bottomRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: true
-  })
-
-  const moveUp = () => {
-    Animated.loop(
-    Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
-    ).start()
-  };
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
 
   [-300, 300]
 
@@ -75,17 +37,26 @@ export default function App() {
     outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"]
   })
 
-  position.addListener(() => console.log(position.getTranslateTransform()))
+  const panResponder = useRef(PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, { dx, dy }) => {
+      position.setValue({
+        x: dx,
+        y: dy
+      })
+      console.log(dx, dy)
+    }
+  })).current
 
   return (
     <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox style={{
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
           transform: [...position.getTranslateTransform()],
           borderRadius,
           backgroundColor
         }} />
-      </Pressable>
     </Container>
   )
 }
