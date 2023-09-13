5c4cb65b0a8c489c10da70a4f393c0f09bdb936a
hyunwoomemo <archihw94@gmail.com>
2023-09-14 01:15:54 +0900

    study: drag & drop 마무리

--- nomad_react_native/React_Native_Master/langApp/App.js
+++ nomad_react_native/React_Native_Master/langApp/App.js
@@ -39,6 +39,7 @@ const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
+  z-index: 10;
 `
 
 const IconCard = styled(Animated.createAnimatedComponent(View))`
@@ -49,35 +50,100 @@ border-radius: 10px;
 
 export default function App() {
   // Values
+  const opacity = useRef(new Animated.Value(1)).current
   const scale = useRef(new Animated.Value(1)).current;
-  const position = useRef(new Animated.ValueXY({x:0, y:0})).current
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: 'clamp'
+  })
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: 'clamp'
+  })
   // Animations
+  const onPressIn = Animated.timing(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  })
+
+  const onPressOut = Animated.timing(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  })
+
+  const goHome = Animated.timing(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  })
+
+  const onDropScale= Animated.timing(scale, {
+    toValue: 0,
+    useNativeDriver: true,
+    duration: 50,
+    easing: Easing.linear,
+  })
+
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    useNativeDriver: true,
+    duration: 50,
+    easing: Easing.linear,
+  })
 
   // Pan Responders
   const panResponder = useRef(PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => {
-      Animated.spring(scale, {
-        toValue: 0.9,
-        useNativeDriver: true
-      }).start()
+      onPressIn.start()
     },
-    onPanResponderMove: () => {
-
+    onPanResponderMove: (_, { dx, dy }) => {
+      console.log(dy)
+      position.setValue({ x: dx, y: dy });
     },
-    onPanResponderRelease: () => {
-      Animated.spring(scale, {
-        toValue: 1,
-        useNativeDriver: true
-      }).start()
+    onPanResponderRelease: (_, { dy }) => {
+      console.log(dy)
+      if (dy < -250 || dy > 250) {
+        Animated.sequence([
+          Animated.parallel([onDropScale,onDropOpacity]),
+          Animated.timing(position, {
+            toValue: 0,
+            duration: 50,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+        ]).start(nextIcon)
+        // onDrop.start()
+      } else {
+        Animated.parallel([
+          onPressOut, goHome
+        ]).start()
+      }
     }
   })).current
   // State
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    Animated.parallel([
+      Animated.spring(scale, {
+        toValue: 1, 
+        useNativeDriver: true
+      }),
+      Animated.spring(opacity, {
+        toValue: 1,
+        useNativeDriver: true
+      })
+    ]).start()
+    setIndex(prev => prev + 1)
+
+  }
 
   return (
     <Container>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
           <Word color={GREEN}>알아</Word>
         </WordContainer>
       </Edge>
@@ -85,16 +151,17 @@ export default function App() {
         <IconCard
           {...panResponder.panHandlers}
           style={{
-          transform: [
-            { scale },
-            ...position.getTranslateTransform()
-          ]
-        }}>
-          <Ionicons name="beer" color={GREY} size={76} />
+            opacity: opacity,
+            transform: [
+              ...position.getTranslateTransform(),
+              { scale },
+            ]
+          }}>
+          <Ionicons name={icons[index]} color={GREY} size={76} />
         </IconCard>
       </Center>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
           <Word color={RED}>몰라</Word>
         </WordContainer>
       </Edge>
