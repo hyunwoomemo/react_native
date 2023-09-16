0fdd84631f392739e03ecf1ebf1794cabb55431a
hyunwoomemo <archihw94@gmail.com>
2023-09-17 03:27:11 +0900

    feat: realm으로 crud 구현하기, 실시간으로 업데이트하기, Layout Animation 사용해서 효과주기

--- nomad_react_native/React_Native_Master/diary/screens/Home.js
+++ nomad_react_native/React_Native_Master/diary/screens/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components/native'
 import colors from '../color';
-import {Ionicons} from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
+import { useDB } from '../context';
+import { FlatList, TouchableOpacity, LayoutAnimation } from 'react-native';
 
 const View = styled.View`
     flex: 1;
     padding: 0 30px;
-    padding-top: 100px;
+    padding-top: 50px;
     background-color: ${colors.bgColor};
   `
 
@@ -31,16 +33,76 @@ const Btn = styled.TouchableOpacity`
   elevation: 5;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `;
-const BtnText = styled.Text`
-  color: white;
+
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  flex-direction: row;
+  padding: 10px 20px;
+  align-items: center;
+  border-radius: 10px;
 `
 
-const Home = ({navigation: {navigate}}) => {
+const Emotion = styled.Text`
+font-size: 24px;
+margin-right: 10px;
+`
+
+const Message = styled.Text`
+font-size: 18px;
+`
+
+const Separator = styled.View`
+  height: 10px;
+`
+
+const Home = ({ navigation: { navigate } }) => {
+  const realm = useDB()
+  const [feelings, setFeelings] = useState()
+  useEffect(() => {
+    const feelings = realm.objects('Feeling')
+    feelings.addListener((feelings, changes) => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+      setFeelings(feelings.sorted("_id", true))
+    });
+
+    return () => {
+      feelings.removeAllListeners();
+    }
+    // filter
+    // const happy = feelings.filtered('emotion = "😀"')
+  }, [])
+
+  const deleteItem = () => {
+    realm.write({
+      // realm.delete
+    })
+  }
+
+  const onPress = (id) => {
+    realm.write(() => {
+      const feeling = realm.objectForPrimaryKey('Feeling', id);
+      realm.delete(feeling)
+    })
+  }
+
+
   return (
     <View>
       <Title>My journal</Title>
+      <FlatList
+        data={feelings} keyExtractor={feeling => feeling._id + ""}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item }) =>
+          <TouchableOpacity onPress={() => onPress(item._id)}>
+            <Record>
+              <Emotion>{item.emotion}</Emotion>
+              <Message>{item.message}</Message>
+            </Record>
+          </TouchableOpacity>
+        } />
       <Btn onPress={() => navigate('Write')}>
-        <Ionicons name="add" color="white" size={40}/>
+        <Ionicons name="add" color="white" size={40} />
       </Btn>
     </View>
   )
