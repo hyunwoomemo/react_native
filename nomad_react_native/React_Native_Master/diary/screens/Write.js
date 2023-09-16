5d5c676906e9745832ec5db943e045473892ca89
hyunwoomemo <archihw94@gmail.com>
2023-09-17 01:18:38 +0900

    write 스크린 코드 수정

--- nomad_react_native/React_Native_Master/diary/screens/Write.js
+++ nomad_react_native/React_Native_Master/diary/screens/Write.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
 import colors from '../color'
+import { Alert } from 'react-native'
 
 const View = styled.View`
     background-color: ${colors.bgColor};
@@ -45,30 +46,46 @@ const Emotions = styled.View`
   margin-bottom: 20px;
 `
 const Emotion = styled.TouchableOpacity`
-  background-color: white;
+  background-color: ${props => props.selected ? '#778beb' : 'white'};
   box-shadow: 1px 1px 5px rgba(0, 0,0,0.1);
   padding: 10px;
   border-radius: 10px;
-`
+  `
 
 const EmotionText = styled.Text`
   font-size: 24px;
 `
 
-const emotions = ["😀", "🥲", "🤬", "🤗", "🥰"]
+const emotions = ["😀", "🥲", "🤬", "🤗", "🥰", "🥳"]
 const Write = () => {
+
+  const [selectedEmotion, setEmotion] = useState(null);
+  const [feelings, setFeelings] = useState("")
+  const onChangeText = (text) => setFeelings(text);
+  const onEmotionPress = (face) => setEmotion(face);
+  const onSubmit = () => {
+    if (feelings === "" || selectedEmotion == null) {
+      return Alert.alert("Please complete form.")
+    }
+  }
   return (
     <View>
       <Title>How do you feel today?</Title>
       <Emotions>
-        {emotions.map((emotion, index) => <Emotion key={index}>
-          <EmotionText>
-            {emotion}
-          </EmotionText>
-        </Emotion>)}
+        {emotions.map((emotion, index) =>
+          <Emotion selected={emotion === selectedEmotion} onPress={() => onEmotionPress(emotion)} key={index}>
+            <EmotionText>
+              {emotion}
+            </EmotionText>
+          </Emotion>)}
       </Emotions>
-      <TextInput placeholder='Write your feelings...' placeholderTextColor="#BCBABA" />
-      <Btn>
+      <TextInput
+        onSubmitEditing={onSubmit}
+        returnKeyType='done'
+        returnKeyLabel='done'
+        autoCapitalize='none'
+        value={feelings} onChangeText={onChangeText} placeholder='Write your feelings...' placeholderTextColor="#BCBABA" />
+      <Btn onPress={onSubmit}>
         <BtnText>Save</BtnText>
       </Btn>
     </View>
